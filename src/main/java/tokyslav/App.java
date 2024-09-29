a32d1fcc8ee8d20baa18b6b5dfd7a926266c107f
maslov02 <s_maslov21@stud.hwr-berlin.de>
2024-09-30 00:21:41 +0200

    first commit

---
+++ src/main/java/tokyslav/App.java
@@ -0,0 +1,41 @@
+package tokyslav;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+    private static final String myText = "HelloText";
+
+    public static void main(String[] args) {
+        System.out.println(myText);
+        createMyWindow();
+    }
+
+    public static String getMyText() {
+        return myText;
+    }
+
+    private static void createMyWindow() {
+        JFrame frame = new JFrame("JFrame Example");
+        JPanel panel = new JPanel();
+        //panel.setLayout(new GridLayout());
+        JLabel label = new JLabel("JFrame By Example");
+        JButton button = new JButton();
+        button.setText("Button");
+        panel.add(label);
+        panel.add(button);
+        frame.add(panel);
+        frame.setSize(200, 300);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+
+}
