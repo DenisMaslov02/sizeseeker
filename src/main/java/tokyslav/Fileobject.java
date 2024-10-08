a663e8a24a07f05e81660a2f2952a30827f222ed
maslov02 <s_maslov21@stud.hwr-berlin.de>
2024-10-08 02:02:04 +0200

    added getter to Fileobject

--- src/main/java/tokyslav/Fileobject.java
+++ src/main/java/tokyslav/Fileobject.java
@@ -4,7 +4,7 @@ public class Fileobject {
     
     private String filename;
     private String size;
-    private FileTypes filetype = FileTypes.DIRECTORY;
+    private FileTypes filetype;
 
     public Fileobject(String p_filename, String p_size, FileTypes p_filetype){
         filename = p_filename;
@@ -12,5 +12,16 @@ public class Fileobject {
         filetype = p_filetype;
     }
 
+    public String getFileName(){
+        return filename;
+    }
+
+    public String getSize(){
+        return size;
+    }
+
+    public FileTypes getFileType(){
+        return filetype;
+    }
 
 }
