2208d1b20ff0836c26511d8921fce309821bc64c
maslov02 <s_maslov21@stud.hwr-berlin.de>
2024-10-08 02:24:33 +0200

    Revert "added FileObjectPanel & demoDisplay"
    This reverts commit 84ea126d0665995d211b62440f780d98da434ad6.

--- src/main/java/tokyslav/filereader/filereader.java
+++ src/main/java/tokyslav/filereader/filereader.java
@@ -1,10 +1,6 @@
 package tokyslav.filereader;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import tokyslav.FileTypes;
-import tokyslav.Fileobject;
+import tokyslav.gui.GUI;
 
 public class filereader {
    
@@ -14,13 +10,6 @@ public class filereader {
         
     }
 
-    public static Fileobject[] getInfoFromPath(String p_pathToSearch){
-        List<Fileobject> fileobjectlist = new ArrayList<Fileobject>();
-        fileobjectlist.add(new Fileobject("Dir_1","3", FileTypes.DIRECTORY));
-        fileobjectlist.add(new Fileobject("Dir_2","6", FileTypes.OTHER));
-        fileobjectlist.add(new Fileobject("Dir_3","3434", FileTypes.FILE));
-        return fileobjectlist.toArray(new Fileobject[0]);
-    }
 
 }
 
