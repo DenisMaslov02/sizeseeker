60355a870be26ca16375b3fd77d63cefa2875642
maslov02 <s_maslov21@stud.hwr-berlin.de>
2024-10-16 23:38:22 +0200

    added easier updateability of centerpanel

--- src/main/java/tokyslav/gui/GUI.java
+++ src/main/java/tokyslav/gui/GUI.java
@@ -22,9 +22,14 @@ public class GUI {
     private final int width = 800;
     private final int height = 600;
     private JFrame frame;
+    private JLabel actualPathJLabel;
+    private JPanel centerPanel;
 
-    private String actualPath = " ";
+    private String actualAbsolutPath = " ";
 
+    /**
+     * @see 
+     */
     public GUI(){
         //constructor of Gui
         frame = new JFrame("SizeSeeker");
@@ -33,52 +38,59 @@ public class GUI {
         frame.setSize(width,height);
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
-        frame.add(headJPanel(),BorderLayout.NORTH);
-        frame.add(centerJPanel(),BorderLayout.CENTER);
-        frame.add(southJPanel(),BorderLayout.SOUTH);
-        
+        createPanels();        
         frame.setVisible(true);
     }
 
-    private JPanel headJPanel(){
+    private void createPanels(){
+        headJPanel();
+        centerJPanel();
+        southJPanel();
+    }
+
+    private void headJPanel(){
         JPanel headPanel = new JPanel();
         headPanel.setSize(width, 200);
-        Dimension d = new Dimension(width,30);
-        headPanel.setPreferredSize(d);
+        headPanel.setPreferredSize(new Dimension(width,30));
         headPanel.setBackground(Color.white);
         headPanel.setLayout(new BorderLayout());
         
         JButton goBackButton = new JButton();
-        goBackButton.setText("DU HURENSOHN");//Zurück
+        goBackButton.setText("Zurück"); //Zurück
         goBackButton.addActionListener(e -> goBackButtonFunction());
         headPanel.add(goBackButton,BorderLayout.LINE_START);
         
-        JLabel actualPathLabel = new JLabel();
-        String textToDisplay = "Du befindest dich hier:" + actualPath;
-        actualPathLabel.setText(textToDisplay);
-        headPanel.add(actualPathLabel,BorderLayout.CENTER);
+        actualPathJLabel = new JLabel();
+        setactualPathOfWindow("1");
+        headPanel.add(actualPathJLabel,BorderLayout.CENTER);
         
-        return headPanel;
+        frame.add(headPanel,BorderLayout.NORTH);
     }
     
     private void goBackButtonFunction(){
-        //System.out.println("Hello u Son of bitch");
-        // opens parent directory and lists all of the files there
-        System.out.println(frame.getWidth());
+        actualAbsolutPath = "2";
+        // actualAbsolutPath = filereader.getParent(actualAbsolutPath);
+        // System.out.println(actualAbsolutPath);
+        setactualPathOfWindow(actualAbsolutPath);
+        frame.remove(centerPanel);
+        centerJPanel();
+    }
+    private void setactualPathOfWindow(String p_path){
+        actualAbsolutPath = p_path;
+        actualPathJLabel.setText("Du befindest dich hier: " + p_path);
     }
 
-    private JPanel centerJPanel(){
-        JPanel centerPanel = new JPanel();
+    private void centerJPanel(){
+        centerPanel = new JPanel();
         centerPanel.setSize(frame.WIDTH, frame.HEIGHT - 100);
         // centerPanel.setPreferredSize(new Dimension(frame.WIDTH, (frame.HEIGHT - 100)));
         centerPanel.setBackground(Color.CYAN);
         centerPanel.setLayout(new BoxLayout(centerPanel, BoxLayout.PAGE_AXIS));
         
-        for (Fileobject i : filereader.getInfoFromPath("hehe")) {
+        for (Fileobject i : filereader.getInfoFromPath(actualAbsolutPath)) {
             centerPanel.add(createFileObjectPanel(i));
         }
-        return centerPanel;
+        frame.add(centerPanel,BorderLayout.CENTER);
     }
     private JPanel createFileObjectPanel(Fileobject tempFileobject){
         JPanel fileObjectPanel = new JPanel();
@@ -95,6 +107,7 @@ public class GUI {
         cornerButton.setSize(20,20);
         cornerButton.setBorder(BorderFactory.createEmptyBorder());
         cornerButton.setContentAreaFilled(false);
+        // cornerButton.addActionListener(e -> goBackButtonFunction());
         fileObjectPanel.add(cornerButton,BorderLayout.LINE_START);
         
         JLabel fileNameLabel = new JLabel(tempFileobject.getFileName());
@@ -125,11 +138,11 @@ public class GUI {
     }
 
     
-    private JPanel southJPanel(){
+    private void southJPanel(){
         JPanel southPanel = new JPanel();
         southPanel.setSize(width, 100);
         southPanel.setBackground(Color.GREEN);
-        return southPanel;
+        frame.add(southPanel,BorderLayout.SOUTH);
     }
 
 }
