6b6714df9cd86a05e8fd4b440a97a2d52441126a
maslov02 <s_maslov21@stud.hwr-berlin.de>
2025-03-06 23:18:06 +0100

    checkpoint march

--- src/main/java/tokyslav/filereader/filereader.java
+++ src/main/java/tokyslav/filereader/filereader.java
@@ -1,6 +1,10 @@
 package tokyslav.filereader;
 
-import tokyslav.gui.GUI;
+import java.util.ArrayList;
+import java.util.List;
+
+import tokyslav.FileTypes;
+import tokyslav.Fileobject;
 
 public class filereader {
    
@@ -10,6 +14,29 @@ public class filereader {
         
     }
 
+    public static Fileobject[] getInfoFromPath(String p_pathToSearch){
+        List<Fileobject> fileobjectlist = new ArrayList<Fileobject>();
+
+        if("1".equals(p_pathToSearch)){
+            fileobjectlist.add(new Fileobject("C:\\","8326583", FileTypes.DRIVE));
+            fileobjectlist.add(new Fileobject("D:\\","6542345", FileTypes.DRIVE));
+            fileobjectlist.add(new Fileobject("E:\\","3949757656", FileTypes.DRIVE));
+        }
+        if("2".equals(p_pathToSearch)){
+            fileobjectlist.add(new Fileobject("Dir_1","28", FileTypes.DIRECTORY));
+            fileobjectlist.add(new Fileobject("Dir_2","45", FileTypes.DIRECTORY));
+            fileobjectlist.add(new Fileobject("config.xml","64", FileTypes.OTHER));
+            fileobjectlist.add(new Fileobject("MeinText.txt","3434", FileTypes.FILE));  
+        }
+        if("3".equals(p_pathToSearch)){
+            fileobjectlist.add(new Fileobject("Dir_1","64792", FileTypes.DIRECTORY));
+            fileobjectlist.add(new Fileobject("MeinText.txt","5", FileTypes.FILE));
+            fileobjectlist.add(new Fileobject("MeinText2.txt","6482", FileTypes.FILE));
+            fileobjectlist.add(new Fileobject("Bachelor.txt","1", FileTypes.FILE));
+        }
+
+        return fileobjectlist.toArray(new Fileobject[0]);
+    }
 
 }
 
