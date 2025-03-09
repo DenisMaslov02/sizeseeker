9b9792b5be27b567c72cae9712ff9877a9539e64
maslov02 <s_maslov21@stud.hwr-berlin.de>
2025-03-09 23:38:58 +0100

    started to sort Fileobjectlist, have to wait for changes

--- src/main/java/tokyslav/gui/GUILogic.java
+++ src/main/java/tokyslav/gui/GUILogic.java
@@ -1,5 +1,23 @@
 package tokyslav.gui;
 
+import java.util.List;
+
+import tokyslav.Fileobject;
+
 public class GUILogic {
     
+    public static Fileobject[] sortItems(List<Fileobject> listOfObjects){
+        List<Fileobject> listToSort = listOfObjects;
+
+        while (!listToSort.isEmpty()){
+            int highestSize = 0;
+            for (Fileobject elem : listToSort) {
+                
+            }
+    
+        }
+
+
+        return null;
+    }
 }
